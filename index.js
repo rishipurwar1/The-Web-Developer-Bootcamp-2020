010b5bf7915836454c883789f3ad8fa20c784e69
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 08:39:13 +0530

    add 28

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+// DO NOT TOUCH!!! (please)
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
 
 // YOUR CODE GOES BELOW THIS LINE:
-planets.shift();
-planets.push('Saturn');
-planets.unshift('Mercury');
+airplaneSeats[3][1]= "Hugo";
