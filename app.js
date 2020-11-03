031caf7a07cdd03e725d9d4c9cc688ee26366c85
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 14:11:24 +0530

    add 59

--- app.js
+++
@@ -1,7 +0,0 @@
-// WRITE YOUR CODE IN HERE:
-for (let i = 0; i < 100; i++) {
-    const newButton = document.createElement("button");
-    newButton.innerText = "Hey";
-    const container = document.querySelector("#container");
-    container.appendChild(newButton);
-}
