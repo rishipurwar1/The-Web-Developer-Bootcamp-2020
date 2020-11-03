a40bee95a37cbe2a90e652c5595ecb4a2630206a
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 13:33:41 +0530

    add 45

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-//NOTE: Udemy's coding exercise platform does NOT support the ** operator.  You'll need to multiply a number by itself or use the Math.pow() method.
-
-const square = function (num) {
-    return Math.pow(num, 2);
-}
+const square = {
+    area: function(x){
+      return x*x;
+    },
+    perimeter: function (x) {
+      return 4*x;
+    }
+  }
