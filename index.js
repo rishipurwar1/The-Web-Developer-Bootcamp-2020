1879f746f145d32f178beb2ca3cdff9f97afbc10
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 13:13:29 +0530

    add 33

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-for (let i=25; i>=0 ; i-=5){
-    console.log(i)
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+people.forEach(name => console.log(name.toUpperCase()));
+
+// Alternative Solution
+for (let i=0; i<people.length; i++) {
+    console.log (people[i].toUpperCase());
 }
