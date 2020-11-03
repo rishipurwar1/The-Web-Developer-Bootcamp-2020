27e4299f2570705eb58eee04c05a9107cec4e075
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 08:31:14 +0530

    add 26

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const mystery = 'Prettier7'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
+const leaderboard = ['Harry', 'Lua', 'Hermoine', 'Bellatrix']; //DON'T TOUCH THIS LINE!
 
-
-// LEAVE THIS CODE ALONE! (pretty please)
-if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
-    console.log("YOU GOT IT!!!");
-}
+// YOUR CODE BELOW HERE
+leaderboard[1] = "Luna";
+leaderboard[3] = "Draco";
