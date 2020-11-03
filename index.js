bbf581eccc8b1d77bd82ec1706c2deedd5b7bc70
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 08:08:52 +0530

    add 15

---
+++ index.js
@@ -0,0 +1,7 @@
+// Define a variable called myLuckyNumber and set it equal to 37
+let myLuckyNumber = 37;
+let octopusLimbs = 8;
+
+// Alternative Solution
+var myLuckyNumber = 37;
+var octopusLimbs = 8;
