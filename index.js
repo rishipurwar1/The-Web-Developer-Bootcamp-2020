1d06f2b7cfa8259cc6f5df80beac29a333d82e47
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 08:18:33 +0530

    add 20

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// DON'T TOUCH THIS LINE! (please)
-const word = 'skateboard' //Don't change this line!
+// NO TOUCHING! (please)
+const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
 
 // YOUR CODE BELOW THIS LINE:
-let facialHair = word.slice(5,10);
-facialHair = facialHair.replace('o','e');
+const roll = `You rolled a ${die1} and a ${die2}. They sum to ${die1+die2}`;
