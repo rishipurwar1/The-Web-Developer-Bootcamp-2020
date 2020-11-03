76e17dbc6079d9e3037f0a70e93fadc8a8fd6e31
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 08:41:30 +0530

    add 29

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-// DO NOT TOUCH!!! (please)
-const airplaneSeats = [
-    ['Ruth', 'Anthony', 'Stevie'],
-    ['Amelia', 'Pedro', 'Maya'],
-    ['Xavier', 'Ananya', 'Luis'],
-    ['Luke', null, 'Deniz'],
-    ['Rin', 'Sakura', 'Francisco']
-];
-
-// YOUR CODE GOES BELOW THIS LINE:
-airplaneSeats[3][1]= "Hugo";
+// Your code goes in here:
+const product = {
+    name: 'Gummy Bears',
+    inStock: true,
+    price: 1.99,
+    flavors: [
+                'grape', 'apple', 'cherry'
+             ]
+}
