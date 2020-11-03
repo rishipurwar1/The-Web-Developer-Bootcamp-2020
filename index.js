a32bf4fdc17f698d0e2040e5047d3f9836e9ecbb
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 13:09:20 +0530

    add 30

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// Your code goes in here:
-const product = {
-    name: 'Gummy Bears',
-    inStock: true,
-    price: 1.99,
-    flavors: [
-                'grape', 'apple', 'cherry'
-             ]
+//PLEASE DON'T TOUCH THIS LINE!
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
 }
+
+const fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`;
