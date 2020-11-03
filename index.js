d4101283459f9ff81d6a1b8251abaf93ab105a2a
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 13:41:51 +0530

    add 49

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const greet = (name) => {
-    return `Hey ${name}!`;
+function validUserNames(strArr){
+	const filterArr = strArr.filter(function(names) {
+	    return names.length < 10;
+        });
+        return filterArr;
 }
+
+// Solution using arrow function
+const validUserNames = (strArr) => {
+    const filterArr = strArr.filter(names => {
+	    return names.length < 10;
+        });
+        return filterArr;
+}
+
+// using implicit return 
+const validUserNames = strArr =>  strArr.filter(names => names.length < 10);
