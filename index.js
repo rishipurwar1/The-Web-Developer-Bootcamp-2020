e667bec9631f811d703d51f27a3500c527d144f4
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 13:31:50 +0530

    add 43

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-function sumArray(arr) {
-    let total = 0;
-    for(let i=0; i<arr.length;i++){
-        total = total+arr[i];
-    }
-    return total;
+const days = {
+    1: 'Monday',
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday"
 }
+ 
+const returnDay = (number) => {
+    if(number < 1 || number > 7) {
+        return null;
+    }
+    return (days[number])
+}
+
+// Alternative Solution
+const returnDay = (num) => {
+    const days = [null, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+      if(num < 1 || num > 7) {
+          return days[0];
+      }
+      return (days[num]);
+  }
