1fa45b1f58bd76b93e278bb1118065ce93c6b513
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 13:36:41 +0530

    add 47

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-let hen = {
-    name: 'Helen',
-    eggCount: 0,
-    layAnEgg() {
-        this.eggCount++;
-        return ('EGG');
-    }
+function cleanNames(arr){
+    const newArr = arr.map(function(str){
+      return str.trim(); 
+   })
+   return newArr;
+}
+
+// Alternative Solution
+const cleanNames = arr => {
+    return arr.map(name => name.trim())
 };
+
+// More cleaner syntax
+const cleanNames = arr => arr.map(name => name.trim());
