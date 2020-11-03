7fb1ce38814df273c9e5b34950a997bc77207dc0
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 13:16:05 +0530

    add 35

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
-
-// WRITE YOUR LOOP BELOW THIS LINE:
-for (let i of numbers) {
-    console.log(i*i);
+// Write your function here:
+function printHeart() {
+    console.log('<3');
 }
+printHeart();
