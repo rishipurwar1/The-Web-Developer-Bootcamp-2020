1b78babccaa80cfa7d79c034827873bb663f66dd
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 13:46:59 +0530

    add 51

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-function allEvens(arr) {
-    let newArray = arr.every(num => num % 2 === 0);
-    return newArray;
-}
-
-// Using arrow function
-const allEvens = arr => {
-    let newArray = arr.every(num => num % 2 === 0);
-    return newArray;
-}
-
-// Using implicit return
-const allEvens = arr => arr.every(num => num % 2 === 0);
+// Write your code in here:
+const image = document.getElementById('unicorn');
+const heading = document.getElementById('mainheading');
