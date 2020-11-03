134a2daa252ff19bd9fa1a2a011b3db973bc3fcd
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 13:55:52 +0530

    add 58

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // WRITE YOUR CODE IN HERE:
-const liS = document.querySelectorAll('li');
- 
-for(let li of liS){
-    li.classList.toggle('highlight');
+for (let i = 0; i < 100; i++) {
+    const newButton = document.createElement("button");
+    newButton.innerText = "Hey";
+    const container = document.querySelector("#container");
+    container.appendChild(newButton);
 }
