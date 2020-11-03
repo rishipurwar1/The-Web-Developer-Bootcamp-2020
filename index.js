02b1bb7e62d66517f4d2acef2a53e5a3b049bde4
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 13:25:54 +0530

    add 40

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // DEFINE YOUR FUNCTION BELOW:
-function isShortsWeather(temperature) {
-    if(temperature >= 75) {
-        return true;
-    } else return false;
+function lastElement(arr) {
+    if(arr.length === 0) {
+        return null;
+    } else {
+        return arr[arr.length - 1];
+    }
 }
+
+// Alternative Solution
+const lastElement = arr => arr.length ? arr[arr.length - 1] : null;
