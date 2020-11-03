019180214c8eafbc861cb88bdd7ce88082776765
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 08:22:26 +0530

    add 22

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function isEven(num){
-    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
-   if(num%2===0) console.log('even');
-    //AND THIS LINE ↑↑↑↑↑
+function getColor(phrase){
+    if(phrase === 'stop'){
+        console.log('red');
+    }
+    else if (phrase === 'slow') {
+        console.log('yellow');
+    }
+    else if (phrase === 'go') {
+        console.log('green');
+    }
+    else {
+        console.log('purple');
+    }
 }
