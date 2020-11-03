9bfe00f46dd53fffa60ca8578b3c9c8d7d110fb8
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 08:24:25 +0530

    add 23

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-function getColor(phrase){
-    if(phrase === 'stop'){
-        console.log('red');
+// Change the value of num, so that "YOU GOT ME!" prints out
+const num = 102; // THIS IS THE ONLY LINE YOU SHOULD CHANGE :) 
+
+// DO NOT TOUCH ANYTHING BELOW (please) 
+if(num <= 100) {
+    if(num >= 50) {
+        console.log("HEY!");
     }
-    else if (phrase === 'slow') {
-        console.log('yellow');
-    }
-    else if (phrase === 'go') {
-        console.log('green');
-    }
-    else {
-        console.log('purple');
+} else {
+    if (num < 103) {
+        if(num % 2 === 0){
+            console.log("YOU GOT ME!");
+        }
     }
 }
