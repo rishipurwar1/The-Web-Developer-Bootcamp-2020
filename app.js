282089c6549af2bdec639eb99d912c9176f4142d
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 14:13:14 +0530

    add 60

---
+++ app.js
@@ -0,0 +1,10 @@
+let hello = document.querySelector("#hello");
+let goodbye = document.querySelector("#goodbye");
+ 
+hello.addEventListener('click', () => {
+    console.log("hello");
+});
+ 
+goodbye.addEventListener('click', () => {
+    console.log("goodbye");
+});
