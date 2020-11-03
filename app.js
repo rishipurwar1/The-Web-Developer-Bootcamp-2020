b6d50f9acea48487435697dd921b44e1075c0a1a
rishipurwar1 <rishipurwar007@gmail.com>
2020-11-03 13:54:23 +0530

    add 57

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
-
-let spans = document.querySelectorAll('span');
-for(let i in colors){
-    spans[i].style.color = colors[i];
-    console.log(i)
+// WRITE YOUR CODE IN HERE:
+const liS = document.querySelectorAll('li');
+ 
+for(let li of liS){
+    li.classList.toggle('highlight');
 }
