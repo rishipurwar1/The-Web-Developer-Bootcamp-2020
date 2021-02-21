82e4800e1a1b7ef5fc767e11facfb82e262706bb
Nagendra M <71176087+nagendram399@users.noreply.github.com>
2021-02-21 13:29:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ for(let i in colors){
     spans[i].style.color = colors[i];
     console.log(i)
 }
+
+#Another Approach
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+for( let i=1;i<=colors.length;i++)
+document.querySelector("span:nth-of-type("+i+")").style.color=colors[i-1];
