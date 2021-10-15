dc981fea0d9cc0ff26df2114a6985f158587fa2a
Ian <iantskon@gmail.com>
2021-10-15 12:21:36 -0500

    Add branch 54

---
+++ app.js
@@ -0,0 +1,4 @@
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+const img = document.querySelector('img');
+img.src = 'https://devsprouthosting.com/images/chicken.jpg';
+img.alt = 'chicken';
