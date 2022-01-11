c5d2d9a42c10f41c80a602ee2f8a5584ec657404
Ian Schoonover <6356890+nax3t@users.noreply.github.com>
2022-01-10 21:19:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-const mystery = 'Prettier7'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
-
-
-// LEAVE THIS CODE ALONE! (pretty please)
-if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
-    console.log("YOU GOT IT!!!");
-}
+// Define lottoNumbers below:
+const lottoNumbers = [1,2,3,4,5,6];
