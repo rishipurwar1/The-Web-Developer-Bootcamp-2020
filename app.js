dbb225bcbe8b8d1127d0a29689ab5f6ebaeb836b
Mrinmoy Chakraborty <81773685+themrinmoy@users.noreply.github.com>
2022-06-10 21:53:07 +0530

    Update app.js
    Alternative method added

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ for(let i in colors){
     spans[i].style.color = colors[i];
     console.log(i)
 }
+//Second Methord
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
+
+for(let i=0; i<colors.length;i++){
+   (document.querySelectorAll('span'))[i].style.color=colors[i]; 
+}
+
+
